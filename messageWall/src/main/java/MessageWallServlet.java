e22676e37e1ed12bc27985d4ad28a8e9f16a3f9a
huang <bokijojo@outlook.com>
2023-07-26 22:43:19 +0800

    5th commit

--- messageWall/src/main/java/MessageWallServlet.java
+++ messageWall/src/main/java/MessageWallServlet.java
@@ -11,13 +11,14 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+class Message{
+    public String from;
+    public String to;
+    public String msg;
+}
 @WebServlet("/message")
 public class MessageWallServlet extends HttpServlet{
-    class Message{
-        public String from;
-        public String to;
-        public String msg;
-    }
+
 
     //暂时用这个变量保存所有数据,后续可能会用数据库保存数据
     private List<Message> messageList=new ArrayList<>();
@@ -29,6 +30,9 @@ public class MessageWallServlet extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Message msg=objectMapper.readValue(req.getInputStream(),Message.class);
+
+        System.out.println(msg.from+msg.msg+msg.to);
+
         messageList.add(msg);
         resp.setStatus(200);
     }
